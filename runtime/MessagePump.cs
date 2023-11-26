90df7c268a845d80d6102c815f343a9375765389
0x6c6565 <0x6c6565@gmail.com>
2023-11-26 10:38:51 -0500

    locks

--- runtime/MessagePump.cs
+++ runtime/MessagePump.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 
 namespace PublishSubscribeSystem
 {
@@ -9,54 +7,74 @@ namespace PublishSubscribeSystem
     {
         Dictionary<Type, Subscription> subscriptions = new Dictionary<Type, Subscription>();
 
+        object publishLock { get; set; } = new object();
+
         public void Publish()
         {
-            foreach (KeyValuePair<Type, Subscription> subscription in subscriptions)
+            lock (publishLock)
             {
-                subscription.Value.Publish();
+                foreach (KeyValuePair<Type, Subscription> subscription in subscriptions)
+                {
+                    subscription.Value.Publish();
+                }
             }
         }
 
         public void Enqueue<T>(in T item) where T : unmanaged, IMessage<T>, IEquatable<T>, IComparable<T>
         {
-            if (subscriptions.ContainsKey(typeof(T)))
+            lock (publishLock)
             {
-                (subscriptions[typeof(T)] as Subscription<T>).Enqueue(item);
+                if (subscriptions.ContainsKey(typeof(T)))
+                {
+                    (subscriptions[typeof(T)] as Subscription<T>).Enqueue(item);
+                }
             }
         }
 
         public void Subscribe<T>(ISubscriber<T> subscriber) where T : unmanaged, IMessage<T>, IEquatable<T>, IComparable<T>
         {
-            if (!subscriptions.ContainsKey(typeof(T)))
+            lock (publishLock)
             {
-                subscriptions.Add(typeof(T), new Subscription<T>());
-            }
+                if (!subscriptions.ContainsKey(typeof(T)))
+                {
+                    subscriptions.Add(typeof(T), new Subscription<T>());
+                }
 
-            Subscription<T> subscription = subscriptions[typeof(T)] as Subscription<T>;
-            subscription.Subscribe(subscriber);
+                Subscription<T> subscription = subscriptions[typeof(T)] as Subscription<T>;
+                subscription.Subscribe(subscriber);
+            }
         }
 
         public void Clear<T>() where T : unmanaged, IMessage<T>, IEquatable<T>, IComparable<T>
         {
-            if (subscriptions.ContainsKey(typeof(T)))
+            lock (publishLock)
             {
-                subscriptions[typeof(T)].Clear();
+                if (subscriptions.ContainsKey(typeof(T)))
+                {
+                    subscriptions[typeof(T)].Clear();
+                }
             }
         }
 
         public void UnsubscribeAll<T>() where T : unmanaged, IMessage<T>, IEquatable<T>, IComparable<T>
         {
-            if (subscriptions.ContainsKey(typeof(T)))
+            lock (publishLock)
             {
-                subscriptions[typeof(T)].UnsubscribeAll();
+                if (subscriptions.ContainsKey(typeof(T)))
+                {
+                    subscriptions[typeof(T)].UnsubscribeAll();
+                }
             }
         }
 
         public void Unsubscribe<T>(ISubscriber<T> subscriber) where T : unmanaged, IMessage<T>, IEquatable<T>, IComparable<T>
         {
-            if (subscriptions.ContainsKey(typeof(T)))
+            lock (publishLock)
             {
-                subscriptions[typeof(T)].Unsubscribe(subscriber);
+                if (subscriptions.ContainsKey(typeof(T)))
+                {
+                    subscriptions[typeof(T)].Unsubscribe(subscriber);
+                }
             }
         }
     }
